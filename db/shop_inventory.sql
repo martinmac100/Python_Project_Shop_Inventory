c5dffef06d89dfa31495e2fd6abdd7608bf07942
matinmac100 <matinmac100@yahoo.co.uk>
2020-10-03 10:31:19 +0100

    db added

---
+++ db/shop_inventory.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS manufacturers;
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    model VARCHAR(255),
+    description VARCHAR(255),
+    colour VARCHAR(255),
+    buy_price FLOAT,
+    sell_price FLOAT
+);
+
+CREATE TABLE manufacturers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    country_of_origin VARCHAR(255)
+
+);
